7418bb27dd792258bd78dd0aa9f9d931058510e1
”sovsianykov” <swebproger@gmail.com>
2021-04-19 16:02:26 +0300

    Detailpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Homepage from "./pages/Homepage";
 import Routing from "./Routing";
-import {BrowserRouter as Router } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Detailpage from "./pages/Detailpage";
 
 
 function App() {
   return (
     <div className="App">
         <Router>
-            <Routing/>
+            <Switch>
+                <Route exact path="/">
+                    <Homepage/>
+                </Route>
+                <Route path="/beer/:id">
+                    <Detailpage/>
+                </Route>
+            </Switch>
+
         </Router>
     </div>
   );
